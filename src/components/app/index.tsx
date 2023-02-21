e8d5370b24af862534b4d8e1cb8eafc159e6ba7c
Mikhail <Mykhailo.Koshmai@dataart.com>
2023-02-21 17:49:17 +0200

    renamed folders

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from '../layout/index'
+import Layout from '../Layout/index'
 import MainPage from '../../pages/MainPage';
 import NotFoundPage from '../../pages/NotFoundPage';
 
